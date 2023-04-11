73f762286af5fc0c96bdb0a644722f5919a7dca0
James <james.crook@fourthfloorcreative.co>
2023-04-11 22:49:26 +0100

    working

--- bin/svelte/generate-svelte.sh
+++ bin/svelte/generate-svelte.sh
@@ -2,6 +2,5 @@
 
 # Create a svelte project
 npm init @svelte-add/kit@latest . -- --with typescript+eslint+prettier
-
 npm install
 # npm run dev -- --open
--- createSvelteApp.js
+++ createSvelteApp.js
@@ -1,7 +1,11 @@
 const { exec } = require('child_process');
+const path = require('path');
 
-const init = () => {
-    exec('sh ./bin/svelte/generate-app.sh', (err, stdout, stderr) => {
+const abc = path.join(__dirname, '/bin/svelte/generate-svelte.sh');
+const dir = path.dirname(abc);
+
+const init = (projectName) => {
+    exec(`sh ${dir}/generate-svelte.sh`, (err, stdout, stderr) => {
         if (err) {
           console.error(err);
           return;
